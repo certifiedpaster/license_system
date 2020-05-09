8b51b31fa72b1ca93331f4974633a5833c51702a
lurumdare <8020186+lurumdare@users.noreply.github.com>
2020-05-09 18:11:12 +0800

    Update Crypto.php

--- test_client/license-system-master/web/Crypto.php
+++ test_client/license-system-master/web/Crypto.php
@@ -5,95 +5,43 @@ class Crypto
 {
     static public function aes_encrypt($input, $key, $iv, $url_encode = false)
     {
-        $command = sprintf("cd %s && ./%s -e \"aes\" -i \"%s\" --key \"%s\" --iv \"%s\"",
-            (Config::Encoder_Path),
+        $command = sprintf("cd %s && ./%s -e aes -i %s --key %s --iv %s",
+            escapeshellarg(Config::Encoder_Path),
             Config::Encoder_Name,
-            ($input),
-            ($key),
-            ($iv)
+            escapeshellarg ($input),
+            escapeshellarg($key),
+            escapeshellarg($iv)
         );
-
+        
         if ($url_encode) {
             $command = sprintf("%s -u", $command);
         }
 
         return exec($command);
-
     }
 
     static public function aes_decrypt($cipher, $key, $iv)
     {
-        $command = sprintf("cd %s && ./%s -d \"aes\" -i \"%s\" --key \"%s\" --iv \"%s\"",
-            (Config::Encoder_Path),
-            Config::Encoder_Name,
-            ($cipher),
-            ($key),
-            ($iv)
-        );
-
-        return exec($command);
-    }
-
-    static public function rsa_encrypt($input, $public_key)
-    {
-        $command = sprintf("cd %s && ./%s -e \"rsa\" -i \"%s\" --public \"%s\"",
+        $command = sprintf("cd %s && ./%s -d aes -i %s --key %s --iv %s",
             escapeshellarg(Config::Encoder_Path),
             Config::Encoder_Name,
-            escapeshellarg($input),
-            escapeshellarg($public_key)
+            escapeshellarg($cipher),
+            escapeshellarg($key),
+            escapeshellarg($iv)
         );
+
         return exec($command);
     }
 
     static public function rsa_decrypt($cipher, $private_key)
     {
-        $command = sprintf("cd %s && ./%s -d \"rsa\" -i \"%s\" --private \"%s\"",
-            (Config::Encoder_Path),
+        $command = sprintf("cd %s && ./%s -d rsa -i %s --private %s",
+           escapeshellarg (Config::Encoder_Path),
             Config::Encoder_Name,
-            ($cipher),
-            ($private_key)
+           escapeshellarg ($cipher),
+            escapeshellarg ($private_key)
         );
 
         return exec($command);
-
-    }
-
-    static public function generate_aes_key_pair()
-    {
-        $command = sprintf("cd %s && ./%s -g",
-            (Config::Encoder_Path),
-            Config::Encoder_Name
-        );
-        exec($command);
-
-        $keypair_file_name = sprintf("%s/keypair.txt", Config::Encoder_Path);
-
-        $file = file_get_contents($keypair_file_name);
-        if (empty($file)) {
-            /// obv. can't find the keypair.txt file
-            return null;
-        }
-
-        $kp = array();
-        $has_key = false;
-        foreach (explode("\n", $file) as $line) {
-            if (empty($line)) {
-                continue;
-            }
-            if (strpos($line, '///') !== false) {
-                continue;
-            }
-
-            if (!$has_key) {
-                $kp["key"] = $line;
-                $has_key = true;
-            } else {
-                $kp["iv"] = $line;
-            }
-        }
-
-        exec(sprintf("rm %s", $keypair_file_name));
-
-        return $kp;
     }
 }
